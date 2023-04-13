a3dbc2865959044a39b72e9b7385983d58e40380
Tam Le <tamsgit@gmail.com>
2023-04-13 06:47:18 +0000

    Add support for using "gpt-3.5-turbo" model (10x cheaper!)

--- main.py
+++ main.py
@@ -862,27 +862,41 @@ SAMPLE_RESPONSE_IF_MAX_RETURN_TOKENS_IS_2000_2 = '''
 : Chat GLM 6B is a 6.2 billion parameter GPT model designed for Chinese dialogue that runs on consumer hardware and is considered one of the best small chat-style GPT models. It uses multi-task training, bi-directional attention, and potentialy Glu activation functions. There are more models coming soon, with opinions varying greatly on which is best.
 '''
 
-DEFAULT_PROMPT = 'Tl;dr'
+DEFAULT_PROMPT = "Tl;dr"
+DEFAULT_MODEL = "text-davinci-003"
 
 MAX_TOKEN_LENGTH = 4097
 
 
-def summarize_text(text, prompt=DEFAULT_PROMPT):
+def summarize_text(text, prompt=DEFAULT_PROMPT, model=DEFAULT_MODEL):
   try:
-    response = openai.Completion.create(model="text-davinci-003",
-                                        prompt=f"{text}\n\n{prompt}",
-                                        temperature=0.7,
-                                        max_tokens=2000,
-                                        top_p=1.0,
-                                        frequency_penalty=0.0,
-                                        presence_penalty=1)
-    return response["choices"][0]["text"]
+    if model == "gpt-3.5-turbo":
+      messages = [{"role": "user", "content": f"{text}\n\n{prompt}"}]
+      response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+      )
+      # print(response)
+      # global r
+      # r = response
+      return response["choices"][0]["message"]["content"]
+    else:
+      response = openai.Completion.create(model=model,
+                                          prompt=f"{text}\n\n{prompt}",
+                                          temperature=0.7,
+                                          max_tokens=2000,
+                                          top_p=1.0,
+                                          frequency_penalty=0.0,
+                                          presence_penalty=1)
+      return response["choices"][0]["text"]
   except openai.error.InvalidRequestError as error:
     if error.user_message.startswith("This model's maximum context length is"):
       text_part_1 = text[:int(len(text) / 2)]
       text_part_2 = text[int(len(text) / 2 + 1):]
-      return summarize_text(
-        summarize_text(text_part_1) + summarize_text(text_part_2))
+      summary_part_1 = summarize_text(text_part_1, model=model)
+      summary_part_2 = summarize_text(text_part_2, model=model)
+      return summarize_text(summary_part_1 + summary_part_2, model=model)
 
 
-print(summarize_text(prompt))
+print(summarize_text(prompt, model="gpt-3.5-turbo"))
